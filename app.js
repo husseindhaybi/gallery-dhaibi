239848aff440d51d63e715ea234a7d2d0fddad53
hussein dhaybi <you@example.com>
2024-03-26 01:26:11 +0200

    ss

---
+++ app.js
@@ -0,0 +1,23 @@
+// const express = require('express')
+// var cors = require('cors')
+// const app = express()
+// app.use(cors())
+// app.get('/', function (req, res) {
+//   res.send('Hello World')
+// })
+
+// app.listen(3000)
+
+const express = require("express");
+var cors = require('cors')
+const app = express();
+app.use(cors())
+app.use(express.static('public'))
+const PORT = process.env.PORT || 3030;
+
+app.get('/',(req,res)=>{
+    res.send(index.html)
+})
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
